419cf43fd70b9a2f63a92b9c8c32693958f05792
zhangzhaoyin <940942500@qq.com>
2016-04-06 17:58:47 +0800

    run修改

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 git pull
 cd app_spider
-nohup python ReadDatabase.py >log/Read.file 2>&1
+nohup python ReadDatabase.py >../log/Read.file 2>&1
 sleep 5
-nohup python ConsumerQQ.py > log/QQout.file 2>&1 &
-nohup python Consumer360.py > log/360out.file 2>&1 &
-nohup python ConsumerBaidumarket.py > log/markout.file 2>&1 &
-nohup python ConsumerBaiduspider.py > log/spider.file 2>&1 &
-nohup python insertData.py > log/inser.file 2 >&1
+nohup python ConsumerQQ.py > ../log/QQout.file 2>&1 &
+nohup python Consumer360.py > ../log/360out.file 2>&1 &
+nohup python ConsumerBaidumarket.py > ../log/markout.file 2>&1 &
+nohup python ConsumerBaiduspider.py > ../log/spider.file 2>&1 &
+nohup python insertData.py > ../log/inser.file 2 >&1
 cd ..
 
